357fd47229954539f264d206b1d0bcdc50dbca07
Balkar_Singh <balkar81199@gmial.com>
2023-01-25 15:25:47 +0530

    feat: add get request handler

--- expressServer.js
+++ expressServer.js
@@ -3,6 +3,12 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 
+let tasks = [];
+let counter = 1;
+app.get('/tasks', (req, res) => {
+  res.json(tasks);
+});
+
 app.listen(process.env.PORT || 3000, ()=> {
   console.log('server started on port 3000');
 });
